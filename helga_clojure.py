318479ed8bf3e2b6918d56e427db3ac92177f7fb
William Dye <will2dye4@mac.com>
2016-11-04 00:58:54 -0400

    Used try-except-else; refactored Popen call to use communicate.

--- helga_clojure.py
+++ helga_clojure.py
@@ -1,6 +1,6 @@
 import os.path
 
-from subprocess import Popen
+from subprocess import Popen, PIPE
 
 from helga.plugins import command
 
@@ -12,11 +12,12 @@ CLOJURE_JAR = os.path.join(os.path.dirname(__file__), 'lib', 'clojure-1.8.0.jar'
          help='')   # TODO
 def clojure(client, channel, nick, message, cmd, args):
     try:
-        java = Popen(['java', '-jar', CLOJURE_JAR, '-e', ' '.join(args)])
-        java.wait()
-        if java.returncode == 0:
-            return java.stdout
-        else:
-            return 'subprocess returned {}, stderr follows: {}'.format(java.returncode, java.stderr)
+        java = Popen(['java', '-jar', CLOJURE_JAR, '-e', ' '.join(args)], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = java.communicate()
     except Exception as e:
         return 'caught exception: {}'.format(str(e))
+    else:
+        if java.returncode == 0:
+            return stdout.strip()
+        else:
+            return 'subprocess returned {}, stderr follows: {}'.format(java.returncode, stderr.strip())
